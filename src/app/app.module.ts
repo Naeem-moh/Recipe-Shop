f8ff590020269f1bfb5e1d4d626eaef833728a30
“naeem-moh” <mohammed.ae.naeem@gmail.com>
2024-05-18 22:33:03 +0300

    structuring - header implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { headerComponent } from './UI/header/header.component';
+import { ShopingListComponent } from './features/shopingList/shoping-list/shoping-list.component';
+import { ShopingListEditComponent } from './features/shopingList/shoping-list-edit/shoping-list-edit.component';
+import { IngredientComponent } from './features/shopingList/ingredient/ingredient.component';
+import { RecipeListComponent } from './features/recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './features/recipeBook/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './features/recipeBook/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent,
+    ShopingListComponent,
+    ShopingListEditComponent,
+    IngredientComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
